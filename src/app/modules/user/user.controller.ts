d160c40a06fb468c61b152567906e5ae59f8eec4
aothymoon59 <aothymoon59@gmail.com>
2023-11-29 22:02:41 +0600

    create validation schema using zod

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express';
 import { UserServices } from './user.service';
+import userValidationSchema from './user.validation';
 
 const createUser = async (req: Request, res: Response) => {
   try {
     const user = req.body;
-    const result = await UserServices.createUserIntoDB(user);
+    // data validation using zod
+    const zodParsedData = userValidationSchema.parse(user);
+
+    const result = await UserServices.createUserIntoDB(zodParsedData);
+
     res.status(200).json({
       success: true,
       message: 'User created successfully!',
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+const userNameValidationSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+});
+
+const userAddressValidationSchema = z.object({
+  street: z.string(),
+  city: z.string(),
+  country: z.string(),
+});
+
+const orderValidationSchema = z.object({
+  productName: z.string(),
+  price: z.number(),
+  quantity: z.number(),
+});
+
+const userValidationSchema = z.object({
+  userId: z.number(),
+  username: z.string(),
+  password: z.string(),
+  fullName: userNameValidationSchema,
+  age: z.number(),
+  email: z.string().email(),
+  isActive: z.boolean(),
+  hobbies: z.array(z.string()),
+  address: userAddressValidationSchema,
+  orders: z.array(orderValidationSchema).optional(),
+});
+
+export default userValidationSchema;
