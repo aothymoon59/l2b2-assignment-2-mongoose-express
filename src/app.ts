c057172002c6fc93c3b886141d279792c6325669
aothymoon59 <aothymoon59@gmail.com>
2023-11-29 20:16:29 +0600

    created user controller,user service and user rouets

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { UserRoutes } from './app/modules/user/user.route';
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 'User app server is running';
+// application routes
+app.use('/api/users', UserRoutes);
 
-  res.send(a);
-});
+const serverController = (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to User Server',
+  });
+};
+app.get('/', serverController);
 
 export default app;
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { UserServices } from './user.service';
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = req.body;
+    const result = await UserServices.createUserIntoDB(user);
+    res.status(200).json({
+      success: true,
+      message: 'User created successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || 'Something went wrong',
+      error: error,
+    });
+  }
+};
+
+export const UserControllers = {
+  createUser,
+};
