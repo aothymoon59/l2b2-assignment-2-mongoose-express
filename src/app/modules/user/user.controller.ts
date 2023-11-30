691f319e56ae94228bec04bcbd7945d61f5069bb
aothymoon59 <aothymoon59@gmail.com>
2023-11-30 19:47:13 +0600

    update user api created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -6,9 +6,9 @@ import { User } from './user.model';
 // create user
 const createUser = async (req: Request, res: Response) => {
   try {
-    const user = req.body;
+    const userData = req.body;
     // data validation using zod
-    const zodParsedData = userValidationSchema.parse(user);
+    const zodParsedData = userValidationSchema.parse(userData);
 
     const result = await UserServices.createUserIntoDB(zodParsedData);
 
@@ -79,8 +79,45 @@ const getSingleUser = async (req: Request, res: Response) => {
   }
 };
 
+// update user by id
+const updateSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const userData = req.body;
+    // check user exists or not
+    const userIsExists = await User.isUserExists(Number(userId));
+    if (!userIsExists) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found',
+        error: {
+          code: 404,
+          description: 'User not found!',
+        },
+      });
+    }
+    const result = await UserServices.updateUserFromDB(
+      Number(userId),
+      userData,
+    );
+    res.status(200).json({
+      success: true,
+      message: 'User updated successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || 'Something went wrong',
+      error: error,
+    });
+  }
+};
+
 export const UserControllers = {
   createUser,
   getAllUsers,
   getSingleUser,
+  updateSingleUser,
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,7 @@
 import { TUser } from './user.interface';
 import { User } from './user.model';
 
+// create user
 const createUserIntoDB = async (userData: TUser) => {
   if (await User.isUserExists(userData.userId)) {
     throw new Error('User already exists');
@@ -9,6 +10,7 @@ const createUserIntoDB = async (userData: TUser) => {
   return result;
 };
 
+// get all user
 const getAllUsersFromDB = async () => {
   const result = await User.find().select({
     username: 1,
@@ -21,7 +23,16 @@ const getAllUsersFromDB = async () => {
   return result;
 };
 
+// get single user by id
 const getSingleUserFromDB = async (userId: number) => {
+  const result = await User.findOne({ userId });
+  return result;
+};
+
+// update user by id
+const updateUserFromDB = async (userId: number, user: TUser) => {
+  await User.updateOne({ userId }, { $set: user }, { new: true });
+
   const result = await User.findOne({ userId });
   return result;
 };
@@ -30,4 +41,5 @@ export const UserServices = {
   createUserIntoDB,
   getAllUsersFromDB,
   getSingleUserFromDB,
+  updateUserFromDB,
 };
